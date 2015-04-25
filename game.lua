7a5dbadf876a05dd0cbad5b78e6e6de3a4f97b9c
alectroemel <alectroemel@hotmail.com>
2015-04-24 20:40:39 -0500

    scrolling map!

--- game.lua
+++ game.lua
@@ -1,21 +1,31 @@
 game = {}
 local sti = require "sti"
 -- Add splash state into the game
-require('menu')
-require('game')
+
 
 function game.load()
-	
-end
 
-
-function game.draw()
-
+	windowWidth = love.graphics.getWidth()
+    windowHeight = love.graphics.getHeight()
+	game.clock = 0 -- time sense game is loaded
 end
 
 function game.update(dt)
-
+ 	-- update map and physics world
+ 	game.clock = game.clock + dt
+    
+    --world:update(dt) 
 end
+
+function game.draw()
+	--love.graphics.scale(scale, scale)
+	for i = 0,8 do -- 6 times vertically 
+		love.graphics.draw(slices["map_normal"], 
+			(i-game.clock%1)*32*scale, 0, 0, scale, scale)
+	end
+end
+
+
 
 function game.keypressed(key)
 
--- main.lua
+++ main.lua
@@ -1,22 +1,44 @@
 loveframes = require("LoveFrames")
+--require('menu')
+require('game')
+
 function love.load()
     -- body
-    local button = loveframes.Create("button")
-    button:SetPos(10, 10)
+    --local button = loveframes.Create("button")
+    --button:SetPos(10, 10)
+    slices_fn = {"map_normal"}
+    slices = {}
+    for _,v in ipairs(slices_fn) do
+        slices[v] = love.graphics.newImage("assets/"..v..".png")
+    end
+
+    -- Set filter to nearest
+    for _,v in pairs(slices) do
+        v:setFilter("nearest", "nearest") -- takes 2 operands on scale up and scale down, makes crisp 
+    end
+    -- Initial State
+    state = "game"
+
+    game.load()
 end
 
 
 function love.update(dt)
 
     -- your code
+    if state == "game" then
+        game.update(dt)
+    end
 
-    loveframes.update(dt)
+    --loveframes.update(dt)
 end
 
 function love.draw()
 
     -- your code
-
+    if state == "game" then
+        game.draw()
+    end
     loveframes.draw()
 
 end
@@ -38,7 +60,9 @@ end
 function love.keypressed(key, unicode)
 
     -- your code
-
+    if state == "game" then
+        game.keypressed(key)
+    end
     loveframes.keypressed(key, unicode)
 end
 
