780e851b8eb6f780e37e9ee9e866cd723ab76c21
alectroemel <alectroemel@hotmail.com>
2015-04-24 20:53:08 -0500

    scrolling.. again

--- game.lua
+++ game.lua
@@ -1,20 +1,22 @@
 game = {}
-local sti = require "sti"
--- Add splash state into the game
-require('menu')
-require('game')
+
+
 
 function game.load()
-	
+	game.clock = 0 -- time sense game is loaded
 end
 
 
 function game.draw()
-
+	for i = 0,8 do -- 6 times vertically 
+		love.graphics.draw(slices["map_normal"], 
+			(i-game.clock%1)*32*scale, 0, 0, scale, scale)
+	end
 end
 
 function game.update(dt)
-
+	-- Update clock for the background
+	game.clock = game.clock + dt
 end
 
 function game.keypressed(key)
