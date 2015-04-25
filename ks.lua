b52a3f5df5a409dfea4002d6147de7a7d088e857
Jmbrace <jmbrace@ilstu.edu>
2015-04-25 14:04:07 -0500

    Commiting kill screen

---
+++ ks.lua
@@ -0,0 +1,64 @@
+loveframes = require("LoveFrames")
+
+ks = {}
+
+function ks.load()
+	
+	ks.dt_temp = 0
+	kScreen = love.graphics.newImage("assets/ksScreen.jpeg")
+	local button = loveframes.Create(button)
+	musicKs = love.audio.newSource("assets/ksMusic.ogg")
+	
+   love.audio.play(musicKs)
+	
+    
+end
+
+function ks.draw()
+	-- draw title image, 32is height of title image,will start right above the screen and move down
+	--			(ks.dt_temp-1)*30*scale	  x	  y position (+ goes down)	0 = rotation
+	
+	love.graphics.draw(kScreen, 0, 0, 0, scale-0.7, scale-0.5)
+	love.graphics.setColor(255, 255, 255)
+    
+    if ks.dt_temp == 2.5 then
+		-- printf will take string and formats it 							center | left | right
+		love.graphics.printf("'R' to Restart", 0, 85*scale, love.graphics.getWidth(), "center")
+		love.graphics.printf("'Q' to Quit", 0, 55*scale, love.graphics.getWidth(), "center")
+	end
+
+   
+	
+	love.graphics.setColor(0,0,0)
+
+end
+
+function ks.update(dt)
+	-- update dt-temp
+	ks.dt_temp = ks.dt_temp + dt
+	-- wait 2.5 seconds, then stop in place
+	if ks.dt_temp > 2.5 then
+		ks.dt_temp = 2.5
+
+	end
+
+
+	
+end
+
+function ks.keypressed(key)
+--change the game state, and return to the menu
+	
+    if key == "r" then 
+	state = "menu"
+	menu.load()
+    end	
+    	if key == "q" then
+      love.event.quit()
+   end
+    
+  
+   
+	--load the menu after 
+	
+end
--- main.lua
+++ main.lua
@@ -1,57 +1,61 @@
 loveframes = require("LoveFrames")
-
+require ('menu')
+require ('ks')
 function love.load()
     -- body
-    local button = loveframes.Create("button")
-    button:SetPos(10, 10)
+    menu.load()
+    --ks.load()
+    --local button = loveframes.Create("button")
+   -- button:SetPos(10, 10)
 end
 
 
 function love.update(dt)
+    --if menu == "menu" then
+    if(state == "menu") then
+        menu.update(dt)
+    end
 
-    -- your code
+    if(state == "ks") then 
+        ks.update(dt)
+    end 
 
     loveframes.update(dt)
+
 end
+
 
 function love.draw()
 
+    -- Set Color
+   -- love.graphics.setColor(bgcolor.r, bgcolor.g, bgcolor.b)
+    -- Draw rectangle for background, fill or line 
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+    -- Return the color back to normal 
+    love.graphics.setColor(255,255,255)
     -- your code
+    if(state=="menu") then
+        menu.draw()
+    --loveframes.draw()
+    end
 
-    loveframes.draw()
-
+    if(state == "ks") then 
+        ks.draw()
+    end 
 end
 
-function love.mousepressed(x, y, button)
+function love.keypressed(key)
+    
+     if(state == "menu") then 
+        menu.keypressed(key) 
+    end
 
-    -- your code
+     if(state == "ks") then 
+        ks.keypressed(key)
+    end
 
-    loveframes.mousepressed(x, y, button)
-end
+    
+    --0 love.graphics.printf("Press Start", 0, 80*scale, love.graphics.getWidth(), "center")
 
-function love.mousereleased(x, y, button)
-
-    -- your code
-
-    loveframes.mousereleased(x, y, button)
-end
-
-function love.keypressed(key, unicode)
-
-    -- your code
-
-    loveframes.keypressed(key, unicode)
-end
-
-function love.keyreleased(key)
-
-    -- your code
-
-    loveframes.keyreleased(key)
-end
-
-function love.textinput(text)
-    -- my code
-
-    loveframes.textinput(text)
+   
 end
